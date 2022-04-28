62ac7f1593bd6da9f4c8ac0ecff84756c8a71a60
vikas <raiv2004322@gmail.com>
2022-04-28 08:24:19 +0530

    Completed the App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,124 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React, { useState } from 'react';
+import { Board } from '../components/Board';
+import { ChoosePlayer } from '../components/ChoosePlayer';
+import { WinnerModal } from '../components/WinnerModal';
+
 
 const Home: NextPage = () => {
+
+  const [isX, setIsX] = useState<boolean>(true);
+  const [newGame, setNewGame] = useState<boolean>(false);
+  const [squares, setSqaures] = useState<Array<any>>(Array(9).fill(null));
+
+  var winner = calculateWinner(squares);
+
+
+  // handle Choose player
+  function handlePlayerX() {
+    setIsX(true);
+  }
+
+  function handlePlayerO() {
+    setIsX(false);
+
+  }
+
+  //// It will Handle which Icon will apppear on Board on cliking  one the Squares
+  function handlePlayer(i: number) {
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = isX ? "X" : "O";
+    setSqaures(squares);
+    setIsX(!isX);
+  }
+
+  // It will handle the Restart of the Game 
+
+  function handleRestartGame() {
+    setIsX(true);
+    setSqaures(Array(9).fill(null));
+  }
+
+
+  // It will handle the start Game when the player choose one of the Icon
+  // with which they want to player
+  function handleNewGame() {
+    setIsX(true);
+    setSqaures(Array(9).fill(null));
+    setNewGame(true);
+  };
+
+  function handleQuitGame() {
+    setIsX(true);
+    setSqaures(Array(9).fill(null));
+    setNewGame(false);
+  }
+  // Calculate the winner
+  function calculateWinner(squares: Array<any>) {
+    // Total 8 winning patterens
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen bg-[#192a32] flex-col items-center  py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Tic-Tic-Toe Game</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <h1 className="text-4xl md:text-6xl font-extrabold mt-4 text-[#30c4bd] ">
+        Tic
+        {" "}
+        <span className="text-[#f3b236]">Tac </span>
+        {" "}
+        Toe
+      </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      {!newGame
+        ?
+        <ChoosePlayer
+          handleNewGame={handleNewGame}
+          handlePlayerX={handlePlayerX}
+          handlePlayerO={handlePlayerO}
+        />
+        :
+        <Board
+          winner={winner}
+          playerX={isX}
+          squares={squares}
+          handlePlayer={handlePlayer}
+          handleRestartGame={handleRestartGame}
+        />
+      }
+      {winner && 
+      <WinnerModal
+          winner={winner}
+          handleQuitGame={handleQuitGame}
+          handleNewGame={handleNewGame}
+        />
+      }
     </div>
   )
 }
